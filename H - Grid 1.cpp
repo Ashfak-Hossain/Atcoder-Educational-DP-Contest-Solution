8b5f5b46dfa28f167ec563ecc16a629e9417c872
Berlin <evan1234.ek@gmail.com>
2024-12-23 22:42:09 +0600

    Refactor H - Grid 1.cpp to implement Bottom-Up approach for path calculation

--- H - Grid 1.cpp
+++ H - Grid 1.cpp
@@ -12,12 +12,11 @@ using namespace std;
 #define debug(...) 42
 #endif
 
-#define endl '\n'
-const int N = 1005;
-const int mod = 1e9 + 7;
+constexpr int MAX_N = 1e3 + 9;
+constexpr int mod = 1e9 + 7;
 
-int n, m, dp[N][N];
-string a[N];
+int n, m, dp[MAX_N][MAX_N];
+string a[MAX_N];
 
 int path(int i, int j) {
   if(i >= n or j >= m) return 0;
@@ -40,7 +39,42 @@ int32_t main() {
     cin >> a[i];
   }
   memset(dp, -1, sizeof dp);
-  cout << path(0, 0) << endl;
+  cout << path(0, 0) << "\n";
+
+  return 0;
+} 
+
+//* Bottom-Up Approach
+
+int dp[MAX_N][MAX_N];
+char a[MAX_N][MAX_N];
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> a[i][j];
+    }
+  }
+
+  dp[0][0] = (a[0][0] != '#');
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (a[i][j] == '#') {
+        dp[i][j] = 0;
+      } else {
+        if (i > 0) dp[i][j] = (dp[i][j] + dp[i - 1][j]) % mod; 
+        if (j > 0) dp[i][j] = (dp[i][j] + dp[i][j - 1]) % mod;
+      }
+    }
+  }
+
+  cout << dp[n - 1][m - 1] << "\n";
 
   return 0;
 } 
